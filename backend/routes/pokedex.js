436e491140d48edc48b005ba8f463219f07febfa
Adrian Sequeira <adrian.sequeira@outlook.com>
2024-10-26 11:39:44 -0600

    Get a pokemon by name feature created

--- backend/routes/pokedex.js
+++ backend/routes/pokedex.js
@@ -26,4 +26,23 @@ router.get('/', async (req, res) => {
     }
 })
 
+/**
+ * @description Gets a Pokemon by name.
+ * @route GET /:name
+ * @returns {Object} 200 - JSON object of the found pokemon.
+ * @returns {Object} 404 - Error message if the pokemon is not found.
+ * @returns {Object} 500 - Error message if something goes wrong.
+ */
+router.get('/:name', (req, res) => {
+    try {
+        db.findOne({ name: req.params.name }, (error, pokemon) => {
+            if (error) return res.status(500).json({ message: error.message })
+            if (!pokemon) return res.status(404).json({ message: 'No pokemon with that name' })
+            res.json(pokemon)
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 module.exports = { pokedexRouter: router }
