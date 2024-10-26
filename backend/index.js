f56ff955137b2dcb0f193bcd6c77550cc6e6dc24
Adrian Sequeira <adrian.sequeira@outlook.com>
2024-10-26 03:19:35 -0600

    Installing neDB and initializing it in the index.js file Is a lightweight database that allow us to run a db similar to mongoDB but without installing any other program

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const Datastore = require('nedb');
+const path = require('path');
 const PORT = 3000
 
 const app = express()
 app.use(express.json())
+
+const db = new Datastore({
+    filename: path.join(__dirname, '/db/pokemons.db'),
+    autoload: true
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
