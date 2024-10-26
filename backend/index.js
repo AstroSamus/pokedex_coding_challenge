d8e98364fa91909963c5465ee60919cc977d38b5
Adrian Sequeira <adrian.sequeira@outlook.com>
2024-10-26 04:09:08 -0600

    Loading pokemons from the pokeApi. When the db is empty we are going to load pokemons from the api by using the dbStarter in the utils folder

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const Datastore = require('nedb');
-const path = require('path');
+const Datastore = require('nedb')
+const path = require('path')
 const PORT = 3000
 
 const app = express()
@@ -10,6 +10,15 @@ const db = new Datastore({
     filename: path.join(__dirname, '/db/pokemons.db'),
     autoload: true
 });
+
+db.count({}, async(error, count) => {
+    if(error) console.log('an error ocurred when counting the db', error)
+    if(count == 0){
+        //if the db is empty, we need to load pokemons from the pokeApi
+        const { pokemonDbStarter } = await import('./utils/dbStarter.js')
+        pokemonDbStarter(db)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
---
+++ backend/utils/dbStarter.js
@@ -0,0 +1,19 @@
+const pokemonDbStarter = async(db) => {
+    const pokeApiRawResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+    const pokeApiResponse = await pokeApiRawResponse.json()
+    for(let pokemon of pokeApiResponse.results) {
+        //individually fetch the information of every pokemon returned by the pokeApi
+        const pokemonDataRawResponse = await fetch(pokemon.url)
+        const pokemonDataResponse = await pokemonDataRawResponse.json()
+        const newDoc = {
+            name: pokemonDataResponse.name,
+            image: pokemonDataResponse.sprites.front_default,
+        }
+    
+        db.insert(newDoc, (err) => {
+            if (err) console.error('error when inserting the new document', err)
+        })
+    }
+}
+
+module.exports = { pokemonDbStarter }
