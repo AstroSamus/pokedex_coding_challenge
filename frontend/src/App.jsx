c8c02ee6abf76d501dd1ea25e7289da525605bd6
Adrian Sequeira <adrian.sequeira@outlook.com>
2024-10-27 16:58:45 -0600

    Searching pokemon by name feature created. Also, moving the logic for the pokedex component to the pokedex component, instead of having it in the app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { Pokedex } from './components/Pokedex/Pokedex'
-import { getAllPokedexInfo } from './services/pokedexService'
 
 function App() {
-  const [pokedexData, setPokedexData] = useState([])
-  useEffect(() => {
-    getPokemonList()
-  }, [])
-
-  const getPokemonList = async() => {
-    setPokedexData(await getAllPokedexInfo())
-  }
 
   return (
     <>
-      <Pokedex pokemonList={ pokedexData }
-      />
+      <Pokedex />
     </>
     
   )
--- frontend/src/components/Pokedex/Pokedex.jsx
+++ frontend/src/components/Pokedex/Pokedex.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react"
 import { PokemonCard } from "../PokemonCard/PokemonCard"
+import { getPokemonByName, getAllPokedexInfo } from "./Pokedex.service"
 
-export const Pokedex = ({ pokemonList }) => {
+export const Pokedex = () => {
+    const [searchBar, setSearchBar] = useState('')
+    const [pokedexData, setPokedexData] = useState([])
+    useEffect(() => {
+      getPokemonList()
+    }, [])
+  
+    const getPokemonList = async() => {
+      setPokedexData(await getAllPokedexInfo())
+    }
+
+    const onSearchPokmemon = async(e) => {
+        e.preventDefault()
+        if(!searchBar) {
+            getPokemonList()
+            return
+        }
+            setPokedexData(await getPokemonByName(searchBar))
+    }
+
     return (
         <div>
             <h1>My Pokedex</h1>
+            <form onSubmit={onSearchPokmemon}>
+                Search a pokemon by name:<input 
+                    type="text" 
+                    value={searchBar}
+                    onChange={(e) => setSearchBar(e.target.value)}/>
+            </form>
             <ul>
-                { pokemonList && pokemonList.map((pokemon) => (
+                { pokedexData && pokedexData.map((pokemon) => (
                     <PokemonCard id={ pokemon._id }
                         key={ pokemon._id }  
                         image = { pokemon.image }
